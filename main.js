a575c70e3b1261bf613001f578302f3ec5d51ac9
dngwoo <akfmzk84@naver.com>
2020-07-31 17:18:01 +0900

    스크롤 방향에 따른 처리 추가, 스크롤의 제일 위, 끝 위치에 따른 처리 추가

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* scroll 시 header 변경 */
+
 const header = document.querySelector(".header");
 
 // 컨텐트 너비, 스크롤바, 패딩 포함
@@ -21,7 +23,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// 해당 section scroll 하기
+/* 해당 section scroll 하기 */
 
 // 방법 1 - 그냥 scrollIntoView 기본 사용법 익힐려고 만듬
 // const sections = document.querySelectorAll("section");
@@ -82,7 +84,7 @@ contactBtn.addEventListener('click', () => {
   });
 })
 
-// 2.2 scroll을 하게 되면 해당 section에 .active 추가 하는 로직
+// 2.2 scroll을 하게 되면 해당 section에 .active 추가 하는 로직. 2가지 방법으로 구현.
 
 const sections = document.querySelectorAll('.section');
 
@@ -98,46 +100,63 @@ const isActive = (activeLink, headerLinks, section) => {
   })
 }
 
+let previousScrollY = 0;
+
 sections.forEach((section, index) => {
   // 방법 1 긱 섹션의 height를 계산해서 window.scrollY와 비교한다.
   // 1-1 각 섹션은 body에서의 정적인 위치를 구한다.
-  // let sectionHeight = 0;
-  // for (let i = 0; i < Array.from(sections).indexOf(sections[index]); i++) {
-  //   sectionHeight += sections[i].getBoundingClientRect().height;
-  // }
+  let sectionHeight = 0;
+  for (let i = 0; i < Array.from(sections).indexOf(sections[index]); i++) {
+    sectionHeight += sections[i].getBoundingClientRect().height;
+  }
 
   document.addEventListener('scroll', () => {
-    // 1-2 각 섹션은 자신의 위치와 window.scrollY가 같거나 작다면  link에 active를 추가한다.
-    //   if (sectionHeight <= window.scrollY) {
-    //     const activeLink = document.querySelector('.header .active');
-    //     const headerLinks = document.querySelectorAll('.header__item span');
-    //     if (activeLink) {
-    //       activeLink.classList.remove('active');
-    //     }
-    //     headerLinks.forEach((headerLink) => {
-    //       const link = headerLink.dataset.link.replace('#', '');
-    //       if (link === section.id) {
-    //         headerLink.classList.add("active");
-    //       }
-    //     })
-    //   }
-    // })
-
-    // 방법 2 dom 탐색기를 사용하여 로직을 짠다.
     const activeLink = document.querySelector('.header .active');
     const headerLinks = document.querySelectorAll('.header__item span');
-    if (index > 0) {
-      if (section.previousElementSibling.getBoundingClientRect().y < 0) {
-        isActive(activeLink, headerLinks, section);
+    // scroll 방향 체크 로직, 1이면 아래로, 0아면 위로
+    const direction = window.scrollY - previousScrollY >= 0 ? 1 : 0;
+    previousScrollY = window.scrollY;
+
+    // 1-2 스크롤 방향에 따라 로직 생성
+    if (direction) { // 스크롤이 아래 방향일 시
+      if (sectionHeight <= window.scrollY) {
+        isActive(activeLink, headerLinks, section)
       }
-    } else {
-      if (section.nextElementSibling.getBoundingClientRect().y > 0) {
+    } else { // 스크롤이 위 방향일 시
+      if ((sectionHeight + section.getBoundingClientRect().height) <= window.scrollY) {
         isActive(activeLink, headerLinks, section)
       }
     }
+
+    // scrollY가 0일 시에는 무조건 Home 메뉴에 .active 추가
+    if (window.scrollY === 0) {
+      if (activeLink) {
+        activeLink.classList.remove('active');
+      }
+      const homeLink = document.querySelector('.header__menu span');
+      homeLink.classList.add('active');
+    }
+
+    // scroll 마지막 부분애는 무조건 Contact 메뉴에 .active 추가
+    console.log(document.body.scrollHeight, window.innerHeight, window.scrollY);
+    if (document.body.scrollHeight === window.innerHeight + window.scrollY) {
+      if (activeLink) {
+        activeLink.classList.remove('active');
+      }
+      const contactLink = document.querySelector('.header__item:last-child span');
+      contactLink.classList.add('active');
+    }
+
+    // 방법 2 dom 탐색기를 사용하여 로직을 짠다. 아직 완성 x
+    // if (index > 0) {
+    //   if (section.previousElementSibling.getBoundingClientRect().y < 0) {
+    //     isActive(activeLink, headerLinks, section);
+    //   }
+    // } else {
+    //   if (section.nextElementSibling.getBoundingClientRect().y > 0) {
+    //     isActive(activeLink, headerLinks, section)
+    //   }
+    // }
   })
 
 })
-
-
-
